65e76d6caa5e2287f91a2e5d64828cb7cbe96a05
Yunior Gonzalez <yunior@squaads.com>
2021-05-14 20:03:04 +0100

    Se han añadido algunos controles de errores. Faltaría pulirlo un poco más. Y falta sobretodo Filtrado y paginado

--- api/customers/customers.controller.js
+++ api/customers/customers.controller.js
@@ -3,42 +3,86 @@ const CustomersModel = require("./customers.model");
 module.exports = { getAll, getOne, deleteOne, putOne, patchOne, postOne };
 
 function getAll(req, res) {
-  return CustomersModel.find().then((customers) => {
-    return res.json(customers);
-  })
+  return CustomersModel.find()
+    .then((customers) => {
+      if (customers.length === 0) {
+        return res.sendStatus(404);
+        return res.status(404).send("No hay customers");
+        return res.status(404).json({ err: "No hay customers" });
+      } else {
+        // Realmente devolver un array vacío si no hay nadie es mejor o "más standar".
+        // Pero en este ejemplo estoy decidiendo devolver 404 como se ve arriba
+        return res.json(customers);
+      }
+    })
+    .catch((e) => {
+      return res.status(500).json(e);
+    });
 }
 
 function getOne(req, res) {
-    const id = req.params.id;
-    return CustomersModel.findById(id).then((customer) => {
+  const id = req.params.id;
+  return CustomersModel.findById(id)
+    .then((customer) => {
       return res.json(customer);
     })
+    .catch((e) => {
+        if (e.kind && e.kind === "ObjectId") {
+        return res
+          .status(400)
+          .send("La string " + id + " no tiene forma de ObjectID");
+      } else {
+        return res.status(500).json(e);
+      }
+    });
 }
 
 function deleteOne(req, res) {
-    const id = req.params.id;
-    return CustomersModel.findByIdAndDelete(id).then((customer) => {
+  const id = req.params.id;
+  return CustomersModel.findByIdAndDelete(id)
+    .then((customer) => {
       return res.json(customer);
     })
+    .catch((e) => {
+      return res.status(500).json(e);
+    });
 }
 
+// TODO: Este no está respondiendo bien. Debería de responder con los datos modificados y no el objeto anterior
 function putOne(req, res) {
-    const id = req.params.id;
-    return CustomersModel.findByIdAndUpdate(id, req.body , { runValidators : true}).then((customer) => {
+  const id = req.params.id;
+  return CustomersModel.findByIdAndUpdate(id, req.body, { runValidators: true })
+    .then((customer) => {
       return res.json(customer);
     })
+    .catch((e) => {
+        return res.status(500).json(e)
+    });
 }
 
+// TODO: Este no está respondiendo bien. Debería de responder con los datos modificados y no el objeto anterior
 function patchOne(req, res) {
-    const id = req.params.id;
-    // El PUT y el PATCH ahora mismo están funcionando igual. Solo editan los atributos que mandas por el body. Tenerlo en cuenta
-    return CustomersModel.findByIdAndUpdate(id, { $set : req.body} , { runValidators : true}).then((customer) => {
+  const id = req.params.id;
+  // El PUT y el PATCH ahora mismo están funcionando igual. Solo editan los atributos que mandas por el body. Tenerlo en cuenta
+  return CustomersModel.findByIdAndUpdate(
+    id,
+    { $set: req.body },
+    { runValidators: true }
+  )
+    .then((customer) => {
       return res.json(customer);
     })
+    .catch((e) => {
+        return res.status(500).json(e)
+    });
 }
 
 function postOne(req, res) {
-    return CustomersModel.create(req.body).then((customer) => {
-      return res.json(customer);
+  return CustomersModel.create(req.body)
+    .then((customer) => {
+      return res.status(201).json(customer);
     })
+    .catch((e) => {
+        return res.status(500).json(e)
+    });
 }
